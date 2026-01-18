aacaba9c32ce3f2dcb6fbd26fa7f3ccdbee4c5de
Matthew Birney <mbirney1975@gmail.com>
2026-01-17 20:28:12 -0500

    Move wiremix from user to system

--- system/configuration.nix
+++ system/configuration.nix
@@ -95,6 +95,7 @@
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     bluetui
     impala
+    wiremix
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
--- users/dvader/home.nix
+++ users/dvader/home.nix
@@ -35,7 +35,6 @@
     vimPlugins.LazyVim
     walker
     wget
-    wiremix
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
